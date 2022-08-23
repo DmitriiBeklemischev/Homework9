9fdffb89cbf11a43c778de6cd9d6337871efd00a
«Dmitrii <Ambal003@ya.ry>
2022-08-23 19:23:53 +0300

    Сделал 2 задачи

---
+++ Task#1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+
+void GetNumber(int m, int n)
+{
+    if (m < n + 1)
+    {
+        Console.Write($" {m},");
+        m++;
+        GetNumber(m, n);
+    }
+
+}
+
+
+Console.WriteLine("Введите число М");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+
+if (m < n)
+{
+    GetNumber(m, n);
+}
+else
+{
+    GetNumber(n, m);
+}
---
+++ Task#2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetSumNumber(int m, int n, int sum)
+{
+
+    if (m < n + 1)
+    {
+        sum += m;
+        m++;
+        return GetSumNumber(m, n, sum);
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число М");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+if (m < n)
+{
+    Console.WriteLine($"{GetSumNumber(m, n, sum)}");
+}
+else
+{
+    Console.WriteLine($"{GetSumNumber(n, m, sum)}");
+}
+
